1857dbc9bf4dd9d654ac2020c6239a76de94af99
Ghanshyam hadiya <160567608+ghanshyamhadiya@users.noreply.github.com>
2025-12-10 19:19:35 +0530

    Fix string syntax for URLs in config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const conf = ({
-    baseUrl: String(https://cloathaura.onrender.com),
-    socketUrl: String(https://cloathaura.onrender.com) || 'http://localhost:8000'
+    baseUrl: String("https://cloathaura.onrender.com"),
+    socketUrl: String("https://cloathaura.onrender.com") || 'http://localhost:8000'
 })
  
 export default conf;
