750598221463ce116386f246ee38a74ef8287bcd
Ghanshyam hadiya <160567608+ghanshyamhadiya@users.noreply.github.com>
2025-12-10 19:15:16 +0530

    Update baseUrl and socketUrl to production URLs

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const conf = ({
-    baseUrl: String(import.meta.env.VITE_BASEURL),
-    socketUrl: String(import.meta.env.VITE_SOCKETURL) || 'http://localhost:8000'
+    baseUrl: String(https://cloathaura.onrender.com),
+    socketUrl: String(https://cloathaura.onrender.com) || 'http://localhost:8000'
 })
  
 export default conf;
