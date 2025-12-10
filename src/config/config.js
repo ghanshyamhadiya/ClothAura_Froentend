9f6c2da01d6565301cbca8c78c316fb3158f31ab
Ghanshyam hadiya <160567608+ghanshyamhadiya@users.noreply.github.com>
2025-12-10 23:17:02 +0530

    Update baseUrl to include '/api' in config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const conf = ({
-    baseUrl: String("https://cloathaura.onrender.com"),
+    baseUrl: String("https://cloathaura.onrender.com/api"),
     socketUrl: String("https://cloathaura.onrender.com") || 'http://localhost:8000'
 })
  
